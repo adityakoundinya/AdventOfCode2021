64c1ab64313adfe22d2f239b8fc7f43bb809daee
Adi Koundinya <akoundinya@wayfair.com>
2021-12-03 11:37:00 -0600

    Adding part 2 of Day 3.

--- 12-03.cs
+++ 12-03.cs
@@ -5,6 +5,7 @@ namespace AdventOfCode{
     public void Run(){
       InputReader inputReader = new InputReader();
       var input = inputReader.GetInputAsString("BinaryDiagnostic.txt");
+
       List<int> ones = new List<int>();
       List<int> zeros = new List<int>();
       foreach(var num in input){
@@ -17,6 +18,7 @@ namespace AdventOfCode{
           else ones[i]++;
         }
       }
+
       StringBuilder gamma = new StringBuilder();
       StringBuilder epsilon = new StringBuilder();
       for(int i = 0; i < ones.Count; i++){
@@ -28,10 +30,49 @@ namespace AdventOfCode{
           epsilon.Append("1");
         }
       }
+
       int g = Convert.ToInt32(gamma.ToString(), 2);
       int e = Convert.ToInt32(epsilon.ToString(), 2);
 
       Console.WriteLine("First: " + g*e);
+
+      int n = input[0].Length;
+
+      List<string> filtered = new List<string>(input);
+      for(int i = 0; i < n; i++){
+        if(filtered.Count <= 1) break;
+        bool isOnes = isOnesGreater(filtered, i);
+        filtered = Filter(filtered, isOnes ? '1' : '0', i);
+      }
+      int o2 = Convert.ToInt32(filtered[0], 2);
+
+      filtered = new List<string>(input);
+      for(int i = 0; i < n; i++){
+        if(filtered.Count <= 1) break;
+        bool isOnes = isOnesGreater(filtered, i);
+        filtered = Filter(filtered, isOnes ? '0' : '1', i);
+      }
+      int co2 = Convert.ToInt32(filtered[0], 2);
+
+      Console.WriteLine("Second: " + o2 * co2);
+    }
+
+    private bool isOnesGreater(List<string> input, int idx){
+      int ones = 0;
+      int zeros = 0;
+      foreach(var num in input){
+        if(num[idx] == '1') ones++;
+        else zeros++;
+      }
+      return ones >= zeros;
+    }
+
+    private List<string> Filter(List<string> input, char filterChar, int idx){
+      List<string> result = new List<string>();
+      foreach(var i in input){
+        if(i[idx] == filterChar) result.Add(i);
+      }
+      return result;
     }
   }
 }
