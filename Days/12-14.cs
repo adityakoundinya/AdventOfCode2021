4985f55359ad55b9191604aa941bcd48c6462688
Adi Koundinya <akoundinya@wayfair.com>
2021-12-14 00:46:20 -0600

    Cleaning up and adding Day 14 Part 2.

--- Days/12-14.cs
+++ Days/12-14.cs
@@ -3,8 +3,15 @@ namespace AdventOfCode {
   public class ExtendedPolymerization : IPuzzle {
     public void Run(){
       InputReader inputReader = new InputReader();
-      var input = inputReader.GetInputAsString("test.txt");
+      var input = inputReader.GetInputAsString("ExtendedPolymerization.txt");
       string template = input[0];
+
+      Dictionary<string, string> map = new Dictionary<string, string>();
+      for(int i = 2; i < input.Count; i++){
+        var temp = input[i].Split("->");
+        map[temp[0].Trim()] = temp[1].Trim();
+      }
+
       Dictionary<string, long> tempMap = new Dictionary<string, long>();
       int p = 0;
       while(p < template.Length - 1){
@@ -13,51 +20,25 @@ namespace AdventOfCode {
         tempMap[temp]++;
         p++;
       }
-      Dictionary<string, string> map = new Dictionary<string, string>();
-      for(int i = 2; i < input.Count; i++){
-        var temp = input[i].Split("->");
-        map[temp[0].Trim()] = temp[1].Trim();
-      }
-      int step = 0;
-      while(step < 40){
-        // StringBuilder sb = new StringBuilder();
-        // int i = 0;
-        // int j = 1;
-        // sb.Append(template[i].ToString());
-        // while(j < template.Length){
-        //   var temp = template[i].ToString() + template[j].ToString();
-        //   if(map.ContainsKey(temp)){
-        //     sb.Append(map[temp] + template[j].ToString());
-        //   }else{
-        //     sb.Append(template[j].ToString());
-        //   }
-        //   i++;
-        //   j++;
-        // }
-        // template = sb.ToString();
+      for(int i = 0; i < 40; i++) {
         tempMap = LookUpAndSplit(tempMap, map);
-        step++;
+        if(i == 9) BuildFreqMapAndPrintResult("First: ", tempMap);
       }
-      // Dictionary<char, long> freqMap = new Dictionary<char, long>();
-      // foreach(var c in template){
-      //   if(!freqMap.ContainsKey(c)) freqMap[c] = 0;
-      //   freqMap[c]++;
-      // }
-      // double result = freqMap.Values.Max() - freqMap.Values.Min();
-      // Console.WriteLine("First: " + result);
-
-      Dictionary<char, long> freqMap2 = new Dictionary<char, long>();
-      foreach(var kvp in tempMap){
-        if(!freqMap2.ContainsKey(kvp.Key[0])) freqMap2[kvp.Key[0]] = 0;
-        freqMap2[kvp.Key[0]] += kvp.Value;
-        if(!freqMap2.ContainsKey(kvp.Key[1])) freqMap2[kvp.Key[1]] = 0;
-        freqMap2[kvp.Key[1]] += kvp.Value;
-      }
-
-      double result = Math.Ceiling((double)(freqMap2.Values.Max() - freqMap2.Values.Min()) / 2);
-      Console.WriteLine("Compare: " + result);
+      BuildFreqMapAndPrintResult("Second: ", tempMap);
     }
 
+    private void BuildFreqMapAndPrintResult(string message, Dictionary<string, long> tempMap){
+      Dictionary<char, long> freqMap = new Dictionary<char, long>();
+      foreach(var kvp in tempMap){
+        if(!freqMap.ContainsKey(kvp.Key[0])) freqMap[kvp.Key[0]] = 0;
+        freqMap[kvp.Key[0]] += kvp.Value;
+        if(!freqMap.ContainsKey(kvp.Key[1])) freqMap[kvp.Key[1]] = 0;
+        freqMap[kvp.Key[1]] += kvp.Value;
+      }
+
+      double result = Math.Ceiling((double)(freqMap.Values.Max() - freqMap.Values.Min()) / 2);
+      Console.WriteLine(message + result);
+    }
     private Dictionary<string, long> LookUpAndSplit(Dictionary<string, long> template, Dictionary<string, string> map){
       Dictionary<string, long> result = new Dictionary<string, long>();
       foreach(var kvp in template){
